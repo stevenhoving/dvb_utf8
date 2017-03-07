664b3fe4f3ede5811c4737a125587fe3bd87a92f
Steven Hoving <stevenhoving@gmail.com>
2017-03-07 08:12:46 +0100

    moved country availibility descriptor to its own file.

---
+++ src/dvb_parser/descriptors/country_availability_descriptor.hpp
@@ -0,0 +1,27 @@
+#ifndef country_availability_descriptor_hpp__
+#define country_availability_descriptor_hpp__
+
+struct country_availability_descriptor : descriptor
+{
+    country_availability_descriptor(const dvb_utf8::stream_buffer &stream)
+        : descriptor(stream)
+    {
+        country_availability_flag = stream.read<uint8_t>();
+        country_code_list.reserve(stream.range_size() / 3);
+        while (!stream.range_eos())
+        {
+            uint32_t country_code =
+                  (uint32_t)stream.read<uint8_t>() << 16
+                | (uint32_t)stream.read<uint8_t>() << 8
+                | (uint32_t)stream.read<uint8_t>();
+
+            country_code_list.emplace_back(country_code);
+        }
+
+        stream.range_mark_end();
+    }
+
+    uint8_t country_availability_flag;          // \note 7 bits
+    std::vector<uint32_t> country_code_list;
+};
+#endif // country_availability_descriptor_hpp__
--- src/dvb_parser/dvb_descriptors.hpp
+++ src/dvb_parser/dvb_descriptors.hpp
@@ -3,29 +3,7 @@
 
 #include "descriptors/descriptors.hpp"
 
-struct country_availability_descriptor : descriptor
-{
-    country_availability_descriptor(const dvb_utf8::stream_buffer &stream)
-        : descriptor(stream)
-    {
-        country_availability_flag = stream.read<uint8_t>();
-        country_code_list.reserve(stream.range_size() / 3);
-        while (!stream.range_eos())
-        {
-            uint32_t country_code =
-                  (uint32_t)stream.read<uint8_t>() << 16
-                | (uint32_t)stream.read<uint8_t>() << 8
-                | (uint32_t)stream.read<uint8_t>();
 
-            country_code_list.emplace_back(country_code);
-        }
-
-        stream.range_mark_end();
-    }
-
-    uint8_t country_availability_flag;          // \note 7 bits
-    std::vector<uint32_t> country_code_list;
-};
 
 struct extended_event
 {
