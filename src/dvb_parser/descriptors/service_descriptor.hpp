071a93087f4923ccfffe714eded64bd7fed9425e
Steven Hoving <stevenhoving@gmail.com>
2017-03-07 08:09:21 +0100

    moved service descriptor to its own file.

---
+++ src/dvb_parser/descriptors/service_descriptor.hpp
@@ -0,0 +1,29 @@
+#ifndef service_descriptor_hpp__
+#define service_descriptor_hpp__
+
+struct service_descriptor : descriptor
+{
+    service_descriptor(const dvb_utf8::stream_buffer &stream)
+        : descriptor(stream)
+    {
+        service_type = stream.read<uint8_t>();
+        uint8_t service_provider_name_length = stream.read<uint8_t>();
+
+        service_provider_name = dvb_utf8::decode(
+            dvb_utf8::stream_buffer(stream.read_buffer(service_provider_name_length)));
+
+        uint8_t service_name_length = stream.read<uint8_t>();
+
+        service_name = dvb_utf8::decode(
+            dvb_utf8::stream_buffer(stream.read_buffer(service_name_length)));
+
+        printf("service_descriptor - provider name: %s, name: %s\n",
+            service_provider_name.c_str(),
+            service_name.c_str()
+        );
+    }
+    uint8_t service_type;
+    std::string service_provider_name;
+    std::string service_name;
+};
+#endif // service_descriptor_hpp__
