a05c981c2bc9f71ec5e639e30a75ff81bb543083
Steven Hoving <stevenhoving@gmail.com>
2017-03-12 15:08:52 +0100

    added network name descriptor. added basic text descriptor base.

--- src/dvb_parser/descriptors/related_content_descriptor.hpp
+++ src/dvb_parser/descriptors/related_content_descriptor.hpp
@@ -1,16 +1,21 @@
 #ifndef related_content_descriptor_hpp__
 #define related_content_descriptor_hpp__
 
-struct related_content_descriptor : descriptor
+/*!
+ * At least 2 descriptors have this basic form.
+ */
+struct basic_text_descriptor_base : descriptor
 {
-    related_content_descriptor(const dvb_utf8::stream_buffer &stream)
+    explicit basic_text_descriptor_base(const dvb_utf8::stream_buffer &stream)
         : descriptor(stream)
     {
-        content_description = dvb_utf8::decode(
+        text = dvb_utf8::decode(
             dvb_utf8::stream_buffer(stream.read_buffer(length)));
-
-        printf("related_content_descriptor: %s\n", content_description.c_str());
     }
-    std::string content_description;
+    std::string text;
 };
+
+using related_content_descriptor = basic_text_descriptor_base;
+using network_name_descriptor = basic_text_descriptor_base;
+
 #endif // related_content_descriptor_hpp__
