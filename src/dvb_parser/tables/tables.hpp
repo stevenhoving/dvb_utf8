4cdf067a1c1ca3cbf684ca3409e35a5b6b1ca036
Steven Hoving <stevenhoving@gmail.com>
2017-03-27 07:53:32 +0200

    move service_descriptor table to its own file.

--- src/dvb_parser/tables/tables.hpp
+++ src/dvb_parser/tables/tables.hpp
@@ -4,6 +4,8 @@
 #include "table_ids.hpp"
 #include "table_base.hpp"
 #include "table_descriptor_container.hpp"
+
 #include "event_information_section.hpp"
+#include "service_description.hpp"
 
 #endif // tables_hpp__
