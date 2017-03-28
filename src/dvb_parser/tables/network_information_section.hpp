ca6311ae3a56df056a17dd46d4da61d53a261169
Steven Hoving <stevenhoving@gmail.com>
2017-03-28 18:01:07 +0200

    fixed network_information_section transport_stream not storing parsed information.

--- src/dvb_parser/tables/network_information_section.hpp
+++ src/dvb_parser/tables/network_information_section.hpp
@@ -5,16 +5,19 @@ namespace dvb_parse
 {
 struct transport_stream : descriptor_container
 {
-    transport_stream(const dvb_utf8::stream_span &stream)
+    explicit transport_stream(const dvb_utf8::stream_span &stream)
     {
-        uint16_t transport_stream_id = stream.read<uint16_t>();
-        uint16_t original_network_ud = stream.read<uint16_t>();
+        transport_stream_id = stream.read<uint16_t>();
+        original_network_ud = stream.read<uint16_t>();
         uint8_t temp = stream.read<uint8_t>();
         uint32_t transport_descriptors_length = (temp & 0x0F) << 8 | stream.read<uint8_t>();
         auto transport_descriptors_stream = stream.read_buffer(transport_descriptors_length);
         while (!transport_descriptors_stream.eos())
             read_descriptor(transport_descriptors_stream);
     }
+
+    uint16_t transport_stream_id;
+    uint16_t original_network_ud;
 };
 
 struct network_information_section : long_section, descriptor_container
