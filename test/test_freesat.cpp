052b1511ecb983cd5fbec2c398bbf81a56bc442a
Steven Hoving <stevenhoving@gmail.com>
2017-04-05 07:37:53 +0200

    fixed freesat test case not creating span correctly.

--- test/test_freesat.cpp
+++ test/test_freesat.cpp
@@ -11,7 +11,7 @@ TEST(test_freesat, test_freesat_decode)
     std::vector<uint8_t> data = { 0x01, 0xaa, 0xbb, 0xc0};
 
     auto decoded_text = freesat::decode(
-        stream_span(&data[0], &data[data.size()]));
+        stream_span(&data[0], &data.data()[data.size()]));
 
     EXPECT_EQ(decoded_text, "Naton");
 }
