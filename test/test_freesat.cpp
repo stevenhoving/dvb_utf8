4547198923a53939e35e2e7f564777220d7b2c61
Steven Hoving <stevenhoving@gmail.com>
2017-04-05 07:40:59 +0200

    added freesat huffman decoding test data from tv_grab_dvb_plus.

--- test/test_freesat.cpp
+++ test/test_freesat.cpp
@@ -1,7 +1,10 @@
 #include "dvb_utf8.hpp"
+#include "test_freesat_test_data.hpp"
+
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 #include <string>
+
 
 using namespace dvb_utf8;
 
@@ -14,4 +17,22 @@ TEST(test_freesat, test_freesat_decode)
         stream_span(&data[0], &data.data()[data.size()]));
 
     EXPECT_EQ(decoded_text, "Naton");
+}
+
+
+
+TEST(test_freesat, test_freesat_decode_tv_grab_dvb_plus_data)
+{
+    for (auto &data : test_data)
+    {
+        auto stream = stream_span(&data[0], &data.data()[data.size()]);
+        uint8_t tableid = stream.read<uint8_t>();
+        uint8_t data_len = stream.read<uint8_t>();
+
+        auto decoded_text = dvb_utf8::decode(stream);
+        printf("%s\n", decoded_text.c_str());
+
+        //EXPECT_EQ(decoded_text, "Naton");
+        EXPECT_FALSE(decoded_text.empty());
+    }
 }
