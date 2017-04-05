99265cf2f474bfc928f050e090d2d5e7e7fd5f91
Steven Hoving <stevenhoving@gmail.com>
2017-04-05 08:05:45 +0200

    added freesat encode & decode unit test.

--- test/test_freesat.cpp
+++ test/test_freesat.cpp
@@ -5,7 +5,6 @@
 #include <gmock/gmock.h>
 #include <string>
 
-
 using namespace dvb_utf8;
 
 /* \note the freesat huffman decoding is incomplete and unstable at best */
@@ -19,8 +18,6 @@ TEST(test_freesat, test_freesat_decode)
     EXPECT_EQ(decoded_text, "Naton");
 }
 
-
-
 TEST(test_freesat, test_freesat_decode_tv_grab_dvb_plus_data)
 {
     for (auto &data : test_data)
@@ -30,9 +27,24 @@ TEST(test_freesat, test_freesat_decode_tv_grab_dvb_plus_data)
         uint8_t data_len = stream.read<uint8_t>();
 
         auto decoded_text = dvb_utf8::decode(stream);
-        printf("%s\n", decoded_text.c_str());
+        //printf("%s\n", decoded_text.c_str());
 
-        //EXPECT_EQ(decoded_text, "Naton");
         EXPECT_FALSE(decoded_text.empty());
     }
 }
+
+#if DVB_UTF8_ENABLE_ENCODE
+TEST(test_freesat, test_freesat_encode)
+{
+    std::string input = "The Country Channel";
+    std::vector<uint8_t> expected{ 0x01, 0x05, 0x0b, 0xfe, 0x86, 0x4d, 0x99, 0xed, 0x08 };
+
+    auto encoded_data = freesat::encode(input, 1);
+    auto expected_data = dvb_utf8::stream_buffer(expected);
+    auto decoded_data = freesat::decode(dvb_utf8::stream_span(encoded_data.data(), &encoded_data.data()[expected.size()]));
+
+
+    EXPECT_EQ(expected_data.data_, encoded_data.data_);
+    EXPECT_EQ(decoded_data, input);
+}
+#endif // DVB_UTF8_ENABLE_ENCODE
